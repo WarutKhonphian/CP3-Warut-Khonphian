f030b6482b9f28fbd4bba2596c99289b07304204
WarutKhonphian <warutk@cheminno.co.th>
2023-10-10 15:59:10 +0700

    Create File Pyhon
    Exercise 4

---
+++ Exercise4_Warut_K.py
@@ -0,0 +1,10 @@
+fEnglish = 69                # Foundation English
+gBusiness =  75            # General Business
+comSys = 89                  # Introduction to Computer Systems
+comProgrram = 80         # Computer Programming
+
+print("--- Your Score ---")
+print("Foundation English : ",fEnglish)
+print("General Business : ",gBusiness)
+print("Introduction to Computer Systems : ",comSys)
+print("Computer Programming : ",comProgrram )
