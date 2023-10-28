193e8af261922ba926674f16edc40b4314361511
WarutKhonphian <warutk@cheminno.co.th>
2023-10-28 16:06:07 +0700

    Lecture50_Warut_K

---
+++ Lecture50_Warut_K.py
@@ -0,0 +1,15 @@
+def resultCalculate(x,y):
+    print("แสดงผล การคำนวนให้รูปแบบต่างๆ")
+    print("ผลบวกของ ",x," กับ ",y ," เท่ากับ ",x+y)
+    print("ผลลบของ ",x," กับ ",y ," เท่ากับ ",x-y)
+    print("ผลคูณของ ",x," กับ ",y ," เท่ากับ ",x*y)
+    print("ผลหารของ ",x," กับ ",y ," เท่ากับ ",x/y)
+    print(20*"-")
+
+number_x = int(input("กำหนดค่าลำดับที่ 1 : "))    
+number_y = int(input("กำหนดค่าลำดับที่ 2 : "))    
+
+
+resultCalculate(number_x,number_y)
+    
+    
